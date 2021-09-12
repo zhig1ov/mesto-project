0153fab5951f18c9fbcc82beb86888ba69b940e5
Artem <zhiglov.artem@yandex.ru>
2021-09-12 20:47:54 +0500

    added form validation

--- scripts/index.js
+++ scripts/index.js
@@ -12,41 +12,7 @@ const popupImageTitle = popupImage.querySelector('.popup__img-name')
 
 const cardItems = document.querySelector('.cards__items')
 
-// Функции для открытия и закрытия попапов =>
 
-function openPopup (popup) {
-  popup.classList.add('popup_opened')
-}
-
-function closePopup (popup) {
-  popup.classList.remove('popup_opened')
-}
-
-//Функциональность для открытия попапов =>
-
-buttonProfileEdit .addEventListener('click', () => {
-  jobInput.value = profileDescription.textContent
-  nameInput.value = profileName.textContent
-  openPopup(popupProfile)
-})
-
-buttonAddCard.addEventListener('click', () => {
-  openPopup(popupAddCard)
-})
-
-//Функциональность для закрытия попапов =>
-
-popupProfileClose.addEventListener('click', () => {
-  closePopup(popupProfile)
-  
-})
-popupCloseAddCard.addEventListener('click', () => {
-  closePopup(popupAddCard)
-})
-
-PopupImageClose.addEventListener('click', () => {
-  closePopup(popupImage)
-})
 
 //Функциональность редактирования профиля =>
 
@@ -55,7 +21,7 @@ const nameInput = document.querySelector('.form__input_type_username')
 const jobInput = document.querySelector('.form__input_type_user-info')
 const profileName = document.querySelector('.profile__name')
 const profileDescription = document.querySelector('.profile__description')
-const popupProfileSaveButton = formPopupEdit.querySelector('.popup__submit-button')
+const popupProfileSaveButton = formPopupEdit.querySelector('.form__submit-button')
 
 popupProfileSaveButton.addEventListener('click', () => {
   closePopup(popupProfile)
@@ -129,5 +95,169 @@ const loadingCard = () => {
 loadingCard()
 
 
+
+
+
+// Функции для открытия и закрытия попапов =>
+
+function openPopup (popup) {
+  popup.classList.add('popup_opened')
+  document.addEventListener('keydown', (evt)=>{
+    if (evt.key === "Escape") {
+        closePopup(popup);
+    }
+  });
+  popup.addEventListener('click', (evt)=>{
+    if (evt.target.classList.contains('popup__body')) {
+      closePopup(popup);
+    }
+  });
+}
+
+function closePopup (popup) {
+  popup.classList.remove('popup_opened')
+  document.removeEventListener('keydown', popup);
+  popup.removeEventListener('click', popup);
+}
+
+//Функциональность для открытия попапов =>
+
+buttonProfileEdit .addEventListener('click', () => {
+  jobInput.value = profileDescription.textContent
+  nameInput.value = profileName.textContent
+  openPopup(popupProfile)
+})
+
+buttonAddCard.addEventListener('click', () => {
+  openPopup(popupAddCard)
+})
+
+//Функциональность для закрытия попапов =>
+
+popupProfileClose.addEventListener('click', () => {
+  closePopup(popupProfile)
+  
+})
+popupCloseAddCard.addEventListener('click', () => {
+  closePopup(popupAddCard)
+})
+
+PopupImageClose.addEventListener('click', () => {
+  closePopup(popupImage)
+})
+
+
+
+//Функция открытия popup для формы редактирования профиля
+
+//Инициализация значений полей и открытия popup редактирования
+function setValueFormEditor () {
+  inputEditProfileName.value = profileName.textContent;
+  inputEditProfileProf.value = profileProfession.textContent;
+  openPopupEvent(popupProfile);
+
+};
+//Раз уж начали popup редактирования юзать, значить надо засабмитить, таков путь
+function submitValueFormProfile (evt) {
+  evt.preventDefault();
+  profileName.textContent = inputEditProfileName.value;
+  profileProfession.textContent = inputEditProfileProf.value;
+  closePopup(popupProfile);
+}
+
+/*Функция, в которой щелчок по карточке должен отобразить ее scaleImagePreview*/
+function openImagePopup (src, alt, name) {
+  placeImageScale.src = src;
+  placeImageScale.alt = alt;
+  placeImageScaleCaption.textContent = name;
+  openPopupEvent(popupImage);
+  console.log(popupImage);
+}
+
+
+
+
+const obj = {
+  formSelector: '.form',
+  inputSelector: '.form__input',
+  submitButtonSelector: '.form__submit-button',
+  fieldsetSelector: '. form__fieldset',
+  inactiveButtonClass: 'form__submit-button_disabled',
+  inputErrorClass: 'form__input_type_error',
+  errorClass: 'form__input-error_visible'
+}
+
+toggleButtonState = (inputList, buttonElement, { inactiveButtonClass }
+) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(inactiveButtonClass);
+    buttonElement.setAttribute("disabled", "disabled");
+  } else {
+    buttonElement.classList.remove(inactiveButtonClass);
+    buttonElement.removeAttribute("disabled", "disabled");
+  }
+};
+
+const showInputError = (formElement, inputElement, errorMessage, { inputErrorClass, errorClass }) => {
+  const errorPlace = formElement.querySelector(`#${inputElement.id}-error`)
+  inputElement.classList.add(inputErrorClass)
+  errorPlace.textContent = errorMessage
+  errorPlace.classList.add(errorClass)
+}
+
+const hideInputError = (formElement, inputElement, {inputErrorClass, errorClass}) => {
+  const errorPlace = formElement.querySelector(`#${inputElement.id}-error`)
+  inputElement.classList.remove(inputErrorClass)
+  errorPlace.textContent = ''
+  errorPlace.classList.remove(errorClass)
+}
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid
+  })
+}
+
+const checkInputValidity = (formElement, inputElement, {...rest}) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage, rest)
+  } else {
+    hideInputError(formElement, inputElement, rest)
+  }
+}
+
+const setEventListeners = (formElement, { inputSelector, submitButtonSelector, ...rest }) => {
+  const inputList = Array.from(formElement.querySelectorAll(inputSelector))
+  const buttonElement = formElement.querySelector(submitButtonSelector)
+  toggleButtonState(inputList, buttonElement, rest)
+  inputList.forEach(inputElement => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement, rest)
+      toggleButtonState(inputList, buttonElement, rest)
+    })
+  })
+}
+
+
+const enableValidation = ({formSelector, fieldsetSelector, ...rest }) => {
+  const formList = Array.from(document.querySelectorAll(formSelector));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function(evt) {
+      evt.preventDefault()
+    })
+    const fieldsetList = Array.from(formElement.querySelectorAll(fieldsetSelector))
+    fieldsetList.forEach(fieldSet => {
+      setEventListeners(fieldSet, rest)
+    })
+  })
+}
+
+enableValidation({formSelector: '.form',
+inputSelector: '.form__input',
+submitButtonSelector: '.form__submit-button',
+fieldsetSelector: '.form__fieldset',
+inactiveButtonClass: 'form__submit-button_disabled',
+inputErrorClass: 'form__input_type_error',
+errorClass: 'form__input-error_visible'})
 
 
--- scripts/validation.js
+++ scripts/validation.js
@@ -1,1 +1,41 @@
-const
+// const form = document.querySelector('.form');
+// const formInput = form.querySelector('.form__input');
+// const formError = form.querySelector(`.${formInput.id}-error`);
+
+
+
+
+// const obj = {
+//   formSelector: '.form',
+//   inputSelector: '.form__input',
+//   submitButtonSelector: '.form__submit',
+//   formError: 'form__input_error'
+// }
+
+// enableValidation(obj);
+
+
+// function enableValidation ({formSelector, inputSelector, submitButtonSelector, formError }) {
+//   forms = Array.from(document.querySelectorAll(formSelector));
+//   forms.forEach(form => {
+//     form.addEventListener('submit', e => e.preventDefault())
+//   })
+
+
+//     const inputs = Array.from(document.querySelectorAll(inputSelector));
+//     inputs.forEach(input => {
+//       input.addEventListener('input', e => {
+//         if(input.validity.valid) {
+//           const inputName = input.getAttribute('name')
+//           const errorPlace = document.getElementById(`${inputName}-error`)
+//           errorPlace.textContent = '';
+//           errorPlace.classList.remove(formError)
+//         } else {
+//           const inputName = input.getAttribute('name')
+//           const errorPlace = document.getElementById(`${inputName}-error`)
+//           errorPlace.textContent = input.validationMessage
+//           errorPlace.classList.add(formError)
+//         }
+//       })
+//     })
+// }
