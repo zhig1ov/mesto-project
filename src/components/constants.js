8bcd99d475f5ed0fff2d27c2c5c4cb71d5f3d5b1
Artem <zhiglov.artem@yandex.ru>
2021-09-18 19:03:45 +0500

    Made webpack assembly

---
+++ src/components/constants.js
@@ -0,0 +1,4 @@
+const popupAddCard = document.querySelector('.popup_type_new-card')
+const popupImage = document.querySelector('.popup_type_image')
+
+export { popupAddCard, popupImage }
---
+++ src/components/index.js
@@ -0,0 +1,34 @@
+import { enableValidation } from './validation.js'
+import { loadingCard, popupImage } from './card.js'
+import { popupProfile, jobInput, nameInput } from './modal.js'
+import '../pages/index.css';
+
+
+//Функциональность редактирования профиля =>
+
+const formPopupEdit = popupProfile.querySelector('.form_type_edit')
+const popupProfileSaveButton = formPopupEdit.querySelector('.form__submit-button')
+
+popupProfileSaveButton.addEventListener('click', () => {
+  closePopup(popupProfile)
+})
+
+function submitFormProfile (evt) {
+  evt.preventDefault()
+  profileName.textContent = nameInput.value
+  profileDescription.textContent = jobInput.value
+}
+
+formPopupEdit.addEventListener('submit', submitFormProfile); 
+
+// Функциональность для добавления карточек =>
+
+enableValidation({formSelector: '.form',
+inputSelector: '.form__input',
+submitButtonSelector: '.form__submit-button',
+fieldsetSelector: '.form__fieldset',
+inactiveButtonClass: 'form__submit-button_disabled',
+inputErrorClass: 'form__input_type_error',
+errorClass: 'form__input-error_visible'})
+
+loadingCard()
